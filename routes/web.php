c33467ee340b1acc1b578e2bb7253e9f15afd496
cokadit <cokadit1@gmail.com>
2020-01-10 17:06:41 +0800

    passing data to views

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,15 @@ Route::view('/', 'welcome');
 Route::view('contact', 'contact');
 Route::view('about', 'about');
 
+Route::get('customers',function() {
+    $customers = [
+        'john doe',
+        'jone doe',
+        'bob the builder'
+    ];
+    return view('internals.customers', [
+        //'customers' dikirim trus diterima sebagai foreach('$customers' as xx$customersxx) di halaman customers.blade
+        'customers' => $customers,
+
+    ]);
+});
