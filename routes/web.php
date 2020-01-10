8c198ba4c385dddec1d56b3089d637d42c7aab27
cokadit <cokadit1@gmail.com>
2020-01-10 17:39:38 +0800

    Blade templating basic

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
-
-// Route::view('url','nama file view.blade.php ');
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::view('/', 'welcome');
+
+
+// Route::view('url','nama file view.blade.php ');
+Route::view('/', 'home');
 Route::view('contact', 'contact');
 Route::view('about', 'about');
 
-Route::get('customers',function() {
-    $customers = [
-        'john doe',
-        'jone doe',
-        'bob the builder'
-    ];
-    return view('internals.customers', [
-        //'customers' dikirim trus diterima sebagai foreach('$customers' as xx$customersxx) di halaman customers.blade
-        'customers' => $customers,
-
-    ]);
-});
+//waktu masuk ke localhost:8000/customers lgsung masuk ke file CustomerController ngejalanin fungsi list
+Route::get('customers', 'CustomerController@list');
